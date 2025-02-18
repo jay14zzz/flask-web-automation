509a6631908f2e8ace94129d861d0104a460e3d4
jay14zzz <91535354+jay14zzz@users.noreply.github.com>
2025-02-18 17:31:12 +0530

    modified config.py with development and production class

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
 # config.py
 
-DEBUG = True
-SECRET_KEY = 'supersecretkey'
 
-RESTRICT_SIGNUP = True
+
+class Config:
+    # Common config settings...
+    SECRET_KEY = 'supersecretkey'
+    RESTRICT_SIGNUP = False
+    
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    PERMANENT_SESSION_LIFETIME = 365 * 24 * 60 * 60  # Sessions never expire in development
+
+class ProductionConfig(Config):
+    DEBUG = False
+    PERMANENT_SESSION_LIFETIME = 3600  # 1 hour timeout for production
